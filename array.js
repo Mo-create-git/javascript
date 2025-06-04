7d710c3b59a0bcefd726ddf7691b02ccdd94ee7e
Mo-create-git <msaffan078@gmail.com>
2025-06-04 11:05:06 +0530

    array's

---
+++ array.js
@@ -0,0 +1,16 @@
+let fruits =  ["apple","Mango","orange"]
+
+console.log(fruits)
+console.log(fruits[0])
+
+let cities = ["hyderabad","chennai","Banglore"]
+console.log(cities)
+console.log(cities[2])
+
+cities[2] = "chennai"//updating array elements.
+
+cities[4] = "Bihar"//ading an array element.
+
+delete cities[2]
+
+console.log(cities)
