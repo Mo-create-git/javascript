b0959f7a12bcc43bdbd738ef9873aa8263d02568
Mo-create-git <msaffan078@gmail.com>
2025-06-15 01:37:17 +0530

    bot project tooo much of concat

---
+++ javascript-functions/programmiz.task02-functions/control-flow/codecamp.org-js/bot-project.js
@@ -0,0 +1,15 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Saffan";
+let botLocation = "Hyderabad";
+let favoriteLanguage = "Urdu";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact;
+ codingFact = "i can code in "+ favoriteLanguage + ".";
+console.log(codingFact);
+codingFact = "one cool thing about me is not everyone can code in " + favoriteLanguage  + "."
+console.log(codingFact);
+codingFact ="i can just code in my " + favoriteLanguage +
+"but it's my mother tongue";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName +  " from " + botLocation + ".");
